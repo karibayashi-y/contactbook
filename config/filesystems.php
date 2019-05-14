ff8ba832d4b22b317d98cf217705253ed98a50ff
haru <h.0706.f@gmail.com>
2019-05-15 00:31:28 +0900

    herokuデプロイ

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => 'ap-northeast-1',
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => 'project-contactbook',
         ],
 
     ],
