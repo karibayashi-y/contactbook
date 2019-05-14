967e6e27c6a56d413252b97a7e5f04af2b0eea77
haru <h.0706.f@gmail.com>
2019-05-15 00:51:09 +0900

    herokuデプロイ

--- config/filesystems.php
+++ config/filesystems.php
@@ -59,8 +59,8 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => 'ap-northeast-1',
-            'bucket' => 'project-contactbook',
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
         ],
 
     ],
